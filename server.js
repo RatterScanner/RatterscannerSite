e04d551c4a05a8557740f844854e059f7f3d23c6
SylusSquared7 <sylussquared@proton.me>
2024-07-23 17:36:37 +0100

    Added dynamic analysis

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const https = require("node:https");
+const multer = require("multer");
+const FormData = require('form-data');
+
+const upload = multer();
 const app = express();
 const loadingGifs = [];
-const https = require('node:https');
 
 app.use(express.static("images"));
 app.set("view engine", "ejs");
@@ -48,5 +52,49 @@ app.get("/report", (req, res) => {
         res.render("report", {completed: completed, gifName: "fadingWord.gif", appID: appID, jsonReport: jsonData});
     });
 })
+
+app.post("/upload", (req, res) => {
+  const upload = multer({ storage: multer.memoryStorage() }); // Store the file in memory
+  const uploadMiddleware = upload.single('jarFile');
+
+  uploadMiddleware(req, res, (err) => {
+    if (err) {
+      return res.status(500).send({ message: 'Error uploading file' });
+    }
+
+    const fileBuffer = req.file.buffer;
+    const fileSize = req.file.size;
+
+    const formData = new FormData();
+    formData.append('file', fileBuffer, {
+      contentType: 'application/octet-stream',
+      filename: req.file.originalname
+    });
+
+    const options = {
+      method: "POST",
+      hostname: "api.ratterscanner.com",
+      path: "/jar_scanner",
+      headers: Object.assign({
+        "api_key": "fdsfadsfjpioy231293u210io312ljnjlasd",
+      }, formData.getHeaders())
+    };
+    let ID;
+
+    const req2 = https.request(options, (res2) => {
+      res2.on("data", (chunk) => { 
+        console.log(`Received chunk: ${chunk}`);
+        const jsonData = JSON.parse(chunk); // parse the response
+        const ID = jsonData.id;
+        res.status(200).send({ message: "Jar file uploaded successfully ID is:", appID: ID });
+      });
+      res2.on("end", () => {
+        console.log("Upload complete");
+      });
+    });
+
+    formData.pipe(req2);
+  });
+});
 
 app.listen(3000);
