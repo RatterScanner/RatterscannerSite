7e8f18313c757f74acda1a45566d9aba577219df
SylusSquared7 <sylussquared@proton.me>
2024-08-05 17:44:56 +0100

    Cleaned up the code and added better comments

--- index.js
+++ index.js
@@ -14,17 +14,57 @@ if (config == undefined) {
   throw new Error(
     "Program Terminated");
 }
+const key = config.apiKey; 
+if (key == null || key == "") { // Checks if the ratterscanner API key is invalid
+  console.error("Key is null because of this the program cannot continue")
+  throw new Error(
+    "Program Terminated");
+}
+
 
 const upload = multer();
 const app = express();
 const fileSizeLimit = config.fileSizeLimit; // The maximum file size an uploaded file can have (In MB)
+const hmacKey = config.altchaKey; // API key to act as an hmac key
 
 app.use(express.static("images"));
 app.set("view engine", "ejs");
 app.use(express.json());
 
 // --------------------------------------------------
+// Put all functions here
 
+const validateCaptcha = async (req) => {
+  const humanKey = req.body["g-recaptcha-response"];
+  console.log("Human key: " + humanKey)
+
+  try {
+    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+      },
+      body: `secret=${config.captchaKey}&response=${humanKey}`
+    });
+
+    const json = await response.json();
+    console.log(JSON.stringify(json))
+    const isHuman = json.success;
+
+    return isHuman;
+  } catch (err) {
+    console.error(`Error in Google Siteverify API: ${err.message}`);
+    return false;
+  }
+};
+
+app.use(function (req, res, next) {
+	res.status(404).render("404")
+});
+
+// --------------------------------------------------
+// Routes only beyond this point
 
 app.get("/", (req, res) => {
     res.render("index", {siteKey: config.siteKey});
@@ -75,13 +115,9 @@ app.get("/report", (req, res) => {
 })
 
 app.post("/upload", upload.single("jarFile"), async (req, res) => {
-  const { jarFile, captchaID, capAns } = req.body;
   const fileBuffer = req.file.buffer;
   const fileSize = req.file.size;
-  const key = config.apiKey;
-  const response_key = req.body["g-recaptcha-response"];
-  const secret_key = config.captchaKey;
-
+  
   console.log("Recieved file")
 
   // Validate captcha
@@ -97,11 +133,6 @@ app.post("/upload", upload.single("jarFile"), async (req, res) => {
 
   if (magicNumber !== '504b0304') { // Check if a file is actually a jar file with magic bytes
     return res.status(400).send({ message: "Invalid JAR file" });
-  }
-
-  if (key == null || key == "") {
-      console.error("ERROR key not read")
-      return res.status(500).send({ message: "Error key is null" });
   }
 
   if (fileSize / (1024 * 1024) > fileSizeLimit) {
@@ -126,7 +157,7 @@ app.post("/upload", upload.single("jarFile"), async (req, res) => {
   let ID;
 
   const req2 = https.request(options, (res2) => {
-    let data = '';
+    let data = "";
   
     res2.on("data", (chunk) => { 
       console.log("Recived chunk: " + chunk)
@@ -138,7 +169,7 @@ app.post("/upload", upload.single("jarFile"), async (req, res) => {
       const jsonData = JSON.parse(data); // parse the response
       let fileSource
   
-      if (jsonData.status == "File found in safe list, not scanning") {
+      if (jsonData.status == "File found in safe list, not scanning") { // The file has been manually marked as safe by a human
         if (Object.keys(jsonData.knownFileDetails.modrinthInfo).length > 0) {
           fileSource = jsonData.knownFileDetails.modrinthInfo.repoUrl;
         } else {
@@ -148,7 +179,9 @@ app.post("/upload", upload.single("jarFile"), async (req, res) => {
         return;
       }
       const downloads = jsonData.knownFileDetails?.modrinthInfo.amountOfDownloads;
-
+      if (downloads == undefined) { // If the file has zero downloads or does not exist on modrinth 
+        downloads = -1
+      }
       const ID = jsonData.id;
       res.status(200).send({ message: "Jar file uploaded successfully ID is:", appID: ID, downloads: downloads});
     });
@@ -157,43 +190,10 @@ app.post("/upload", upload.single("jarFile"), async (req, res) => {
   formData.pipe(req2);
 });
 
-const validateCaptcha = async (req) => {
-  const humanKey = req.body["g-recaptcha-response"];
-  console.log("Human key: " + humanKey)
-
-  try {
-    const response = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
-      method: 'post',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-      },
-      body: `secret=${config.captchaKey}&response=${humanKey}`
-    });
-
-    const json = await response.json();
-    console.log(JSON.stringify(json))
-    const isHuman = json.success;
-
-    return isHuman;
-  } catch (err) {
-    console.error(`Error in Google Siteverify API: ${err.message}`);
-    return false;
-  }
-};
-
 app.get("/safe", function (req, res) {
   const data = JSON.parse(req.query.data);
   
   res.render("safe", {fileName: data.fileName, downloadLink: data.fileDownload});
-});
-
-app.get("/test", function (req, res) {
-  res.render("test", {siteKey: config.siteKey})
-})
-
-app.use(function (req, res, next) {
-	res.status(404).render("404")
 });
 
 app.listen(config.port);
